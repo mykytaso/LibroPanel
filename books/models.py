92847f3922b5cf5bfdeefe1f7d65c9ecc71261aa
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2024-10-08 13:18:34 -0400

    Add max_digits=10 to daily_fee field in Book model

--- books/models.py
+++ books/models.py
@@ -11,4 +11,4 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     cover = models.CharField(max_length=50, choices=CoverChoices.choices)
     inventory = models.PositiveIntegerField(unique=True)
-    daily_fee = models.DecimalField(decimal_places=2)
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
