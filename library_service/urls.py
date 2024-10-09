56f0e5aa5bc7d8c961cb212e856961bd6b125a8c
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2024-10-09 14:11:32 -0400

    Include users/urls.py in project's main urls.py

--- library_service/urls.py
+++ library_service/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("books.urls", namespace="books")),
+    path("api/", include("users.urls", namespace="users")),
 ]
